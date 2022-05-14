26f812384e652bd02d49c1950f37654aa6f114d2
shiliang <mirstone@126.com>
2022-05-14 23:04:48 +0800

    模块化配置

--- main/src/main/java/com/sl/main/MainApp.java
+++ main/src/main/java/com/sl/main/MainApp.java
@@ -4,11 +4,30 @@ import android.util.Log;
 
 import com.sl.base.BaseApplication;
 
+import java.lang.reflect.Field;
+
 public class MainApp extends BaseApplication {
     private static final String TAG = "MainApp";
+
     @Override
     public void initModuleApp(BaseApplication application) {
-        Log.d(TAG, "initModuleApp: ");
+        String className = application.getPackageName() + ".BuildConfig";
+        try {
+            Class<?> aClass = Class.forName(className);
+            Object obj = aClass.newInstance();
+            Field field = aClass.getDeclaredField("BASE_URL");
+            field.setAccessible(true);
+            String url = (String) field.get(obj);
+            Log.d(TAG, "initModuleApp: " + url);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
